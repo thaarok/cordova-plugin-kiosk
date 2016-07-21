e23296ec9f30a995b5441b640497d3f128eb2f9f
Jan Kalina <jkalina@redhat.com>
2016-07-21 13:07:41 +0200

    isSetAsLauncher() + activity label by MainActivity

--- android/KioskPlugin.java
+++ android/KioskPlugin.java
@@ -2,6 +2,7 @@ package jk.cordova.plugin.kiosk;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import org.apache.cordova.*;
 import android.widget.*;
@@ -20,8 +21,8 @@ import org.json.JSONObject;
 public class KioskPlugin extends CordovaPlugin {
     
     public static final String EXIT_KIOSK = "exitKiosk";
-    
     public static final String IS_IN_KIOSK = "isInKiosk";
+    public static final String IS_SET_AS_LAUNCHER = "isSetAsLauncher";
     
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -31,12 +32,18 @@ public class KioskPlugin extends CordovaPlugin {
                 callbackContext.success(Boolean.toString(KioskActivity.running));
                 return true;
                 
+            } else if (IS_SET_AS_LAUNCHER.equals(action)) {
+                
+                String myPackage = cordova.getActivity().getApplicationContext().getPackageName();
+                callbackContext.success(Boolean.toString(myPackage.equals(findLauncherPackageName())));
+                return true;
+                
             } else if (EXIT_KIOSK.equals(action)) {
                 
                 Intent intent = new Intent(Intent.ACTION_MAIN);
                 intent.addCategory(Intent.CATEGORY_HOME);
                 
-                Intent chooser = Intent.createChooser(intent, "Select destination...");                
+                Intent chooser = Intent.createChooser(intent, "Select destination...");
                 if (intent.resolveActivity(cordova.getActivity().getPackageManager()) != null) {
                     cordova.getActivity().startActivity(chooser);
                 }
@@ -51,6 +58,13 @@ public class KioskPlugin extends CordovaPlugin {
             callbackContext.error(e.getMessage());
             return false;
         }
+    }
+    
+    private String findLauncherPackageName() {
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        final ResolveInfo res = this.cordova.getActivity().getPackageManager().resolveActivity(intent, 0);
+        return res.activityInfo.packageName;
     }
 }
 
--- kiosk.js
+++ kiosk.js
@@ -17,6 +17,18 @@ var KioskPlugin = {
         }, function(error){
             alert("KioskPlugin.isInKiosk failed: " + error);
         }, "KioskPlugin", "isInKiosk", []);
+    },
+    
+    isSetAsLauncher: function (callback) {
+        if(/ios|iphone|ipod|ipad/i.test(navigator.userAgent)) {
+            callback(false); // ios not supported - cannot be in kiosk
+            return;
+        }
+        exec(function(out){
+            callback(out == "true");
+        }, function(error){
+            alert("KioskPlugin.isSetAsLauncher failed: " + error);
+        }, "KioskPlugin", "isSetAsLauncher", []);
     }
     
 }
